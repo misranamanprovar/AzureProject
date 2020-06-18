0d9bc6628b393704557e6af604d9273261108bfc
naman misra <misranaman@gmail.com>
2020-06-18 10:16:26 +0530

    Ant Zip Update

--- ProvarHome/lib/com.provar.plugins.sf.apis_2.205.28.0/resources/javascript/Lightning_SetValuesForSF1.js
+++
@@ -1,7 +0,0 @@
-var webElement = arguments[1]
-var auraId = webElement.getAttribute("data-aura-rendered-by");
-var component = auraId ? $A.getComponent(auraId) : null;
-
-if(component && component.getAttributeValueProvider()) {
-	component.getAttributeValueProvider().set('v.value', arguments[0]);
-}
--- ProvarHome/lib/com.provar.plugins.sf.apis_2.205.28.0/resources/javascript/ListUploadFileAction_complete.js
+++
@@ -1,6 +0,0 @@
-var generatedFileElement = arguments[0];
-if(generatedFileElement && generatedFileElement instanceof Node) {
-	document.body.removeChild(generatedFileElement);
-}
-delete window._provarFileButton;
-
